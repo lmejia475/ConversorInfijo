ab4215756a9af6e792f88471321afeb7df1cd6fb
luis mejia <luis03mejia03@gmail.com>
2025-02-25 00:13:34 -0600

    Mejoras en vista

--- PilaBlazor/Services/Pila.cs
+++ PilaBlazor/Services/Pila.cs
@@ -57,47 +57,9 @@ namespace PilaBlazor.Services
             return pila.Pop(); 
         }
 
-        public int MAX { get; set; }
 
-        public int TOPE { get; set; }
 
-        public int Valor { get; set; }
 
-        public Pila(int max)
-        {
-            MAX = max;
-            Elementos = new string[MAX];
-            TOPE = 0;
-        }
-
-        public bool IsEmpty => TOPE == 0;
-
-        public bool IsFull => TOPE == Elementos.Length;
-
-        public void Push(string valor)
-        {
-            if (IsFull) return;
-
-            Elementos[TOPE] = valor;
-            TOPE++;
-        }
-
-        public string Pop()
-        {
-            if (IsEmpty) return "";
-
-            TOPE--;
-
-            return Elementos[TOPE];
-        }
-
-        //public IEnumerator GetEnumerator()
-        //{
-        //    for (int i = TOPE - 1; i >= 0; i--)
-        //    {
-        //        yield return Elementos[i];
-        //    }
-        //}
     }
 
 }
